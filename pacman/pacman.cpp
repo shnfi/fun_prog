52f4181f14e50a573acabb8130d46ed287da81a7
shnfi <mrplom7@gmail.com>
2025-01-14 17:46:11 +0330

    added a very simple movement system for enemies

--- pacman/pacman.cpp
+++ pacman/pacman.cpp
@@ -20,10 +20,14 @@ public :
 	int speed;
 	char sprite;
 	int health;
+	char direction;
 
-	void render(int p_y, int p_x)
+	void render(char role, int p_y, int p_x)
 	{
-		mvprintw(p_y, p_x, " ");
+		if (role == 'e')
+			mvprintw(p_y, p_x, ".");
+		else if (role == 'p')
+			mvprintw(p_y, p_x, " ");
 
 		mvprintw(this->y, this->x, "%c", this->sprite);
 		refresh();
@@ -48,7 +52,7 @@ public :
 		this->direction = 'd';
 		this->score = 0;
 
-		this->render(this->y, this->x);
+		this->render('p', this->y, this->x);
 	}
 
 	void move()
@@ -125,7 +129,7 @@ public :
 			}
 		}
 
-		this->render(p_y, p_x);
+		this->render('p', p_y, p_x);
 
 		mvprintw(1, 54, "p: %d", this->score);
 
@@ -152,6 +156,63 @@ public :
 		}
 		else
 			mvprintw(1, 57, "%d / 336", this->score);
+	}
+};
+
+class ENEMY : public CHARACTER
+{
+public :
+	ENEMY(int x, int y, int speed, char sprite, int health, char direction)
+	{
+		this->x = x;
+		this->y = y;
+		this->speed = speed;
+		this->sprite = sprite;
+		this->health = health;
+		this->direction = direction;
+
+		this->render('e', this->y, this->x);
+	}
+
+	void move()
+	{
+		int p_y = this->y;
+		int p_x = this->x;
+
+		if (this->direction == 'a' && this->x == 1 && (this->y == 14 || this->y == 15))
+		{
+			this->x = 49;
+
+			if (this->y == 14)
+				this->y  = 14;
+			else
+				this->y = 15;
+		}
+		else if (this->direction == 'd' && this->x == 49 && (this->y == 14 || this->y == 15))
+		{
+			this->x = 1;
+
+			if (this->y == 14)
+				this->y  = 14;
+			else
+				this->y = 15;
+		}
+		else
+		{
+			if (this->direction == 'w' && (map[this->y - 1][this->x] == ' ' || map[this->y - 1][this->x] == '.'))
+				this->y -= this->speed;
+
+			else if (this->direction == 'a' && (map[this->y][this->x - 2] == ' ' && !(this->x == 33 && (this->y == 14 || this->y == 15)) || map[this->y][this->x - 2] == '.'))
+				this->x -= this->speed * 2; // because we multiply the map's width to 2 (for more beautiful render)
+
+			else if (this->direction == 's' && (map[this->y + 1][this->x] == ' ' || map[this->y + 1][this->x] == '.'))
+				this->y += this->speed;
+
+			else if (this->direction == 'd' && (map[this->y][this->x + 2] == ' ' || map[this->y][this->x + 2] == '.'))
+				this->x += this->speed * 2; // because we multiply the map's width to 2 (for more beautiful render)
+		}
+
+		this->render('e', p_y, p_x);
 	}
 };
 
@@ -193,12 +254,15 @@ int main()
 	 * CHARACTER ROLES :
 	 *
 	 * player -> it is the main character of the game, its you!
-	 * enemy1 -> it is the normal enemy with 1 health and a normal speed. 
-	 * enemy2 -> it is the normal enemy with 2 health and a normal speed. 
-	 * enemy3 -> it is the normal enemy with 2 health and a high speed. 
+	 * normal_enemy -> it is the normal enemy with 1 health and a normal speed.
+	 * tough_enemy -> it is the normal enemy with 2 health and a normal speed.
+	 * satan_enemy -> it is the normal enemy with 2 health and a high speed.
 	 */
 
 	PLAYER player(1, 1, 1, 'O', 3);
+
+	ENEMY normal_enemy1(33, 14, 1, 'X', 1, 'd');
+	ENEMY normal_enemy2(17, 14, 1, 'X', 1, 'a');
 
 	while (!_abort)
 	{
@@ -210,6 +274,9 @@ int main()
         }
 
         player.move();
+
+		normal_enemy1.move();
+		normal_enemy2.move();
 
 		refresh();
 
